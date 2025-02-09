cb44b8fc03a02575a11b7e5a1b676c7c2ebe2bad
Uhut <uhut.sancar@diamandtech.com>
2025-02-09 16:46:23 +0300

    first  commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./main/static/header/header.component";
+import { FooterComponent } from "./main/static/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'MotionMeet.web';
---
+++ src/app/main/static/header/header.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterLinkActive, RouterLink ,CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+  encapsulation: ViewEncapsulation.None
+})
+export class HeaderComponent {
+
+
+
+
+}
